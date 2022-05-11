34a2122d7463ad190de3c08d2f6053de3cdcae0e
hafizdarmawan <hafizdarmawan53@gmail.com>
2022-05-11 13:22:15 +0700

    Laravel dan Tailwind CSS

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->when(request()->search, function ($posts) {
+            $posts = $posts->where('title', 'like', '%' . request()->search . '%');
+        })->paginate(5);
+
+        return view('post.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:png,jpeg,jpg',
+            'title'     => 'required',
+            'content'   => 'required'
+        ]);
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        if ($post) {
+            return redirect()->route('post.index')->with(['success' => 'Data Berhasil Disimpan']);
+        } else {
+            return redirect()->route('post.index')->with(['error' => 'Data Gagal Disimpan!']);
+        }
+    }
+
+    public function edit(Post $post)
+    {
+        return view('post.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $this->validate($request, [
+            'title'     => 'required',
+            'content'   => 'required'
+        ]);
+
+        // get data post by ID
+        $post = Post::findOrFail($post->id);
+        if ($request->file('image') == "") {
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        } else {
+            // hapus image old
+            Storage::disk('local')->delete('public/posts/' . $post->image);
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content
+            ]);
+        }
+
+        if ($post) {
+            //redirect dengan pesan sukses
+            return redirect()->route('post.index')->with(['success' => 'Data Berhasil Diupdate!']);
+        } else {
+            //redirect dengan pesan error
+            return redirect()->route('post.index')->with(['error' => 'Data Gagal Diupdate!']);
+        }
+    }
+
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        Storage::disk('local')->delete('public/posts/' . $post->image);
+        $post->delete();
+
+        if ($post) {
+            //redirect dengan pesan sukses
+            return redirect()->route('post.index')->with(['success' => 'Data Berhasil Dihapus!']);
+        } else {
+            //redirect dengan pesan error
+            return redirect()->route('post.index')->with(['error' => 'Data Gagal Dihapus!']);
+        }
+    }
+}
